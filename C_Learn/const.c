2e4d13ffcead670c330a1b2babf2848b082358cf
zxx_cmd <1981834783@qq.com>
2021-09-26 08:00:12 +0800

    new changes

---
+++ C_Learn/const.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    const int a = 12;
+    printf("%d\n", a);
+    // *p = 13;
+    printf("%d", a);
+    system("pause");
+    return 0;
+}
